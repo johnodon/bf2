0e77187414061b0dc197454ed551727446b0eb2a
johnodon <johnodon@hotmail.com>
2019-09-07 11:53:34 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,8 +2,11 @@
 
 echo "---BF2 Check---"
 if [ ! -f "${SERVER_DIR}/start.sh" ]; then
-    echo "---BF2 not installed, extracting!---"
+    echo "---BF2 not installed, downloading!---"
     cd /serverfiles
+    wget -nc -q --show-progress --progress=bar:force:noscroll ftp://ftp.bf-games.net/server-files/bf2/bf2-linuxded-1.5.3153.0-installer.tgz
+    wget -nc -q --show-progress --progress=bar:force:noscroll https://www.bf2hub.com/downloads/BF2Hub-Unranked-Linux-R3.tar.gz
+    echo "---Extracting!---"
     chmod +x /opt/scripts
     /opt/scripts/extract
 fi
