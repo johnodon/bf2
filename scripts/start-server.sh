9b713c8bd33447f5fb642123a07bd7404ee9aafc
johnodon <johnodon@hotmail.com>
2019-09-07 08:13:08 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,13 +4,13 @@ echo "---BF2 Check---"
 if [ ! -f "${SERVER_DIR}/start.sh" ]; then
     echo "---BF2 not found, downloading!---"
     cd ${INSTALLER_DIR}
-    wget -q - ftp://ftp.bf-games.net/server-files/bf2/bf2-linuxded-1.5.3153.0-installer.tgz
+    wget ftp://ftp.bf-games.net/server-files/bf2/bf2-linuxded-1.5.3153.0-installer.tgz
     tar -xvf bf2-linuxded-1.5.3153.0-installer.tgz
     rm -R bf2-linuxded-1.5.3153.0-installer.tgz
 fi
 
 echo "---Prepare Server---"
-chmod +x $INSTALLER_DIR
+chmod +x $INSTALLER_DIR /opt/scripts
 ./opt/scripts/extract
 
 fi
@@ -21,6 +21,4 @@ find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-screen -S Terraria -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/TerrariaServer.bin.x86_64 ${GAME_PARAMS}
-sleep 2
-tail -f ${SERVER_DIR}/masterLog.0
+./start.sh
