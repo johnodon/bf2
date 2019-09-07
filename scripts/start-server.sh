b9e369d100a1615b1575093590838d0726a15fab
johnodon <johnodon@hotmail.com>
2019-09-07 09:04:11 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,20 +3,20 @@
 echo "---BF2 Check---"
 if [ ! -f "${SERVER_DIR}/start.sh" ]; then
     echo "---BF2 not found, downloading!---"
-    cd ${INSTALLER_DIR}
+    cd /serverfiles
     wget ftp://ftp.bf-games.net/server-files/bf2/bf2-linuxded-1.5.3153.0-installer.tgz
     tar -xvf bf2-linuxded-1.5.3153.0-installer.tgz
     rm -R bf2-linuxded-1.5.3153.0-installer.tgz
     echo "---Prepare Server---"
-    chmod +x $INSTALLER_DIR /opt/scripts
+    chmod +x /opt/scripts
     ./opt/scripts/extract
 fi
 
 echo "---Server ready---"
 chmod -R 770 ${DATA_DIR}
 echo "---Checking for old logs---"
-find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
+find /serverfiles/bf2 -name "masterLog.*" -exec rm -f {} \;
 
 echo "---Start Server---"
-cd ${SERVER_DIR}
+cd /serverfiles/bf2
 ./start.sh
