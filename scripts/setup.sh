adc336a29fd69eeda258c273ee497f2bede7737f
johnodon <johnodon@hotmail.com>
2019-09-18 17:57:10 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,25 +19,21 @@ if [ ! -f "/serverfiles/BF2Hub-Unranked-Linux-R3.tar.gz" ]; then
     echo "---BF2hub source not found, downloading!---"
     wget -nc -q --show-progress --progress=bar:force:noscroll https://www.bf2hub.com/downloads/BF2Hub-Unranked-Linux-R3.tar.gz
 fi
-
 if [ ! -f "/serverfiles/BF2CCD_1.4.2446.zip" ]; then
     echo "---BF2CC source not found, downloading!---"
     wget -nc -q --show-progress --progress=bar:force:noscroll https://www.fullcontactwar.com/files/BF2CCD_1.4.2446.zip
     unzip BF2CCD_1.4.2446.zip –d /serverfiles/bf2
 fi
-
 if [ ! -f "/serverfiles/bf2/bf2ccd.exe" ]; then
     echo "---BF2CC files not found, extracting!---"
     unzip BF2CCD_1.4.2446.zip –d /serverfiles/bf2
 fi
-
 if [ ! -f "/serverfiles/mono-1.1.12.1_0-installer.bin" ]; then
     echo "---mono installer source not found, downloading!---"
     wget -nc -q --show-progress --progress=bar:force:noscroll https://download.mono-project.com/archive/1.1.12.1/linux-installer/0/mono-1.1.12.1_0-installer.bin
     chmod +x mono-1.1.12.1_0-installer.bin
     /serverfiles/mono-1.1.12.1_0-installer.bin
 fi
-
 if [ ! -f "/serverfiles/mono-1.1.12.1_0-installer.bin" ]; then
     echo "---mono installer source not found, downloading!---"
     chmod +x mono-1.1.12.1_0-installer.bin
